0513dbd03133641583881b5badc22654f2a34b6d
Edmendez-dev <eduardodev00@gmail.com>
2026-06-23 01:07:44 -0600

    feat(tech section): Adds a section with basic skills, in a timeline style, as well as language support (#4)

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/hero";
 import { FloatingDock } from "@/components/floatingDock";
 import { Projects } from "@/components/projects";
+import { TechStack } from "@/components/techStack";
 import { Contact } from "@/components/contact";
 import { Footer } from "@/components/footer";
 
@@ -11,6 +12,7 @@ export default function Home() {
       <main>
         <Hero />
         <Projects />
+        <TechStack />
         <Contact />
       </main>
       <Footer />
